3714ddb645248015998504722639377cd9c10dce
fjortegan@gmail.com <c@bras12>
2021-10-25 23:08:33 +0200

    Ejemplo de insercion de imagen

--- codigophp/ejemplos/imagen-mod.php
+++ codigophp/ejemplos/imagen-mod.php
@@ -1,10 +1,25 @@
 <?php
+    //checks query for custom text
+    $text = isset($_GET["text"])? $_GET["text"] : 'I wanna be a developer';
+
+    //load image
     $im = imagecreatefromjpeg("dog.jpg");
 
+    //response will be a jpeg image
     header('Content-Type: image/jpeg');
 
-    imagestring($im, 1, 10, 10, "Hola, soy yo", 1);
+    //choose color
+    $blue = imagecolorallocate($im, 0x14, 0x36, 0x42);
 
+    //ruta archivo de fuente ttf
+    $font_file = './Lora.ttf';
+    
+    //draws text with size 40
+    imagefttext($im, 36, 0, 40, 100, $blue, $font_file, $text);
+
+    //write image data in response
     imagejpeg($im);
+
+    //destroy image object
     imagedestroy($im);
 ?>
